235b4b9a13e2fc9dda44b03e9c572b23d5e2a36e
cawilson1 <cawilson1@g.cofc.edu>
2020-04-26 13:55:28 -0400

    add url params

--- src/App.js
+++ src/App.js
@@ -6,12 +6,18 @@ const App = () => (
   <div>
     <h1>Tutorial!</h1>
     <nav>
-      <Link to="/">Home</Link> <Link to="dashboard">Dashboard</Link>
+      <Link to="/">Home</Link>
+      <Link to="dashboard">Dashboard</Link>
     </nav>
     <Router>
       <Home path="/" />
       <Dashboard path="dashboard" />
+      <Invoice path="invoices/:invoiceId" />
     </Router>
+
+    <Link to="invoices/123">Invoice 123</Link>
+    {"   "}
+    <Link to="invoices/hello">Invoice Hello</Link>
   </div>
 );
 
